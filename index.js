15702c513a0a5be8fbd71d4497cbada379092a6b
HoodFast <rabota-trassa@mail.ru>
2023-08-06 00:26:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+const express = require('express');
+const bodyParser= require('body-parser')
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.mail.ru",
+    secure: true,
+    port: 465,
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    },
+    tls: {rejectUnauthorized: false}
+})
+
+
+app.post('/sendMessage', async function (req, res) {
+
+    let info = await transporter.sendMail({
+            from: 'rabota-trassa@mail.ru',
+            to: 'rabota-trassa@mail.ru',
+            subject: 'Test gmail',
+            html: `<div> 
+
+    <div><span>name : </span><span>${req.body.name}</span></div>
+    <div><span>mail : </span><span>${req.body.email}</span></div>
+    <div><span>message : </span><span>${req.body.message}</span></div>
+</div>`
+        }, (err, success) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('OK')
+            }
+        }
+    )
+});
+
+
+let port = process.env.PORT || 3000
+
+app.listen(port, function () {
+    console.log('Server start')
+})
