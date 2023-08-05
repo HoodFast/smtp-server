b48d0ed91d4690a762e63d370fb25a4d18869a26
HoodFast <rabota-trassa@mail.ru>
2023-08-06 00:32:54 +0300

    commit

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/sendMessage', async function (req, res) {
 });
 
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 80
 
 app.listen(port, function () {
     console.log('Server start')
